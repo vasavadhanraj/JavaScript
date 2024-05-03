d305e9fcf062d26ca7e150179c534b6b20896f91
vasavadhanraj <vasavadhanraj26@gmail.com>
2024-05-03 08:29:16 +0530

    Update file

--- Console-prompt/prompt.js
+++ Console-prompt/prompt.js
@@ -2,6 +2,6 @@ let FirstName = prompt("Hello , Enter your First Name.");
 let LastName = prompt("Enter your Last Name.");
 let Email = prompt("Enter your Email.");
 console.log(`Hii, ${FirstName} ${LastName} Welcome to the Website.`);
-console.log(Email);
+console.log(`This is your Email ID :${Email.toLowerCase()}`);
 console.error("Error Occured");
 console.warn("Warning");

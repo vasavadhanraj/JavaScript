8906355ecd99cc057787004f71f7f19644c40a7e
vasavadhanraj <vasavadhanraj26@gmail.com>
2024-05-04 09:50:23 +0530

    Update file

--- Array/array.js
+++ Array/array.js
@@ -85,3 +85,59 @@ console.log(numbers.reverse());//For Original Array
 let str = "Shivam";
 str[0] = "a";
 console.log(str);
+
+//Slice
+let fruits1 = ["apple","banana","orange","mango","pineapple"]
+console.log(fruits1.slice(1,3));
+console.log(fruits1.slice(2));
+console.log(fruits1.slice(-3));
+console.log(fruits1.slice(1,-2));
+console.log(fruits1.slice());
+console.log(fruits1.slice(fruits1.length-1));
+console.log(fruits1.slice(fruits1.length));
+console.log(fruits1.slice(5));
+console.log(fruits1);
+
+//Splice (Splice Change in Original Array)
+let fruits2 = ["apple","banana","orange","mango","pineapple","Grapes","Chiku"];
+console.log(fruits2.splice(5));
+console.log(fruits2);
+console.log(fruits2.splice(0,1));
+console.log(fruits2);
+console.log(fruits2.splice(0,1,"Guava","Papaya"));
+console.log(fruits2);
+//splice(StartIndex,DeleteCount.Item0...ItemN)
+console.log(fruits2.splice(1,0,"Dragonfruit","Watermelon"));
+console.log(fruits2);
+
+//sort
+console.log(fruits2.sort())
+let nums = [1,2,3,4,5,6,7,8,9,10];
+console.log(nums.sort());
+
+//Array References
+let arr = ['a','b','c'];
+let arrCopy = arr;
+console.log(arrCopy);
+
+arr.push('d');
+console.log(arr);
+console.log(arrCopy);
+arrCopy.pop();
+console.log(arr);
+console.log(arrCopy);
+
+//const Array (can't reuse the name of array)
+const arr1 = ['a','b','c'];
+// arr1 = ['d','e','f'];
+arr1.push('d');
+console.log(arr1);
+// arr1 = ['d','e','f'];
+console.log(arr1);
+
+//Nested Array (Multi Dimension Array)
+let ArrayMulti = [[2,4],[3,6],[4,8]];
+console.log(ArrayMulti);
+console.log(ArrayMulti.length);
+console.log(ArrayMulti[0].length);
+console.log(ArrayMulti[0][1]);

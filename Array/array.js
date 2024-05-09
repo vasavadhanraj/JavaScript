380dbeee25cdbf35bb60d1d886218f760ac1ad3f
vasavadhanraj <vasavadhanraj26@gmail.com>
2024-05-09 08:39:48 +0530

    New file

--- Array/array.js
+++ Array/array.js
@@ -111,7 +111,7 @@ console.log(fruits2.splice(1,0,"Dragonfruit","Watermelon"));
 console.log(fruits2);
 
 //sort
-console.log(fruits2.sort())
+console.log(fruits2.sort());
 let nums = [1,2,3,4,5,6,7,8,9,10];
 console.log(nums.sort());
 

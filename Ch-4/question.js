5b6d0fa9b9ad39170e2ef1905aea19f9e4e941a7
vasavadhanraj <vasavadhanraj26@gmail.com>
2024-05-10 09:53:45 +0530

    New file

---
+++ Ch-4/question.js
@@ -0,0 +1,31 @@
+//Question 1
+
+console.log("Question 1")
+
+for(let i = 0;i <= 20;i++){
+    console.log(i);
+}
+
+console.log("Question 2")
+
+for(let i = 3;i <= 29;i++){
+    if(i%2==1){
+        console.log(i);
+    }
+}
+
+console.log("Question 3")
+
+for(let i = 12;i >= -14;i--){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+console.log("Question 4")
+
+for(let i = 50;i >= 20;i--){
+    if(i%3==0){
+        console.log(i);
+    }
+}

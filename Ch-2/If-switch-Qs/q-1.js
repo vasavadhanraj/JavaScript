080c32081e1d3b743e1186a7fd9ba964e6e0fa6b
vasavadhanraj <vasavadhanraj26@gmail.com>
2024-05-18 10:33:03 +0530

    New file

---
+++ Ch-2/If-switch-Qs/q-1.js
@@ -0,0 +1,7 @@
+let item = prompt("Hello, Enter any item Starting with Alphabet 'a'.");
+
+if((item[0] =="a") && (item.length > 3)){
+    console.log("It is a Golden String.");
+} else{
+    console.log("It is not a Golden String.");
+}
